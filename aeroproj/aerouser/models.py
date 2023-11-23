cbe3077a19a9d2eeab32c839ef4f8a24482d89b2
Venukanthan BS <114436303+itsvenu22@users.noreply.github.com>
2023-11-23 17:43:59 +0530

    Added files for Email Authentication
    User signup and login with email verification

---
+++ aeroproj/aerouser/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class userdata(models.Model):
+    username=models.CharField(max_length=255,null=True)
+    email = models.CharField(max_length=255,null=True)
+    password = models.CharField(max_length=255,null=True)
+    otp = models.CharField(max_length=255,null=True)
+    
