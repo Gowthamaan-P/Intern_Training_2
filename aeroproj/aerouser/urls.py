c68f83f1088523ffe8fe2d7fd18c5165a473a111
Shiyam DT <141911803+Sub-tachyon@users.noreply.github.com>
2023-11-24 19:42:23 +0530

    Added files for Login (Forgot password)
    User can change password via forgot password.

--- aeroproj/aerouser/urls.py
+++ aeroproj/aerouser/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+
 from . import views
 
 urlpatterns = [
     path("login", views.login, name="login"),
     path("signup",views.signup, name="signup" ),
+    path("forgot/",views.forgot, name="forgot"),
+    path("otp_verification",views.otp, name="otp_verification"),
+    path("password_change/<str:token>",views.password_reset, name="password_change"),
 ]
