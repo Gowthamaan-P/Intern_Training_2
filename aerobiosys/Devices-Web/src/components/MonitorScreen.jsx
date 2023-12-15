6a82c4fc87741c8dadc8d4477e0a12ed0aa114a0
Its-Suryakumar <suryakumar36531@gmail.com>
2023-12-15 11:48:50 +0530

    Implemented Live Graph Sample from JSON

--- aerobiosys/Devices-Web/src/components/MonitorScreen.jsx
+++ aerobiosys/Devices-Web/src/components/MonitorScreen.jsx
@@ -1,16 +1,24 @@
 import React, {useState, useEffect} from 'react'
+import './MonitorScreen.css'
 import SocketIOClient from 'socket.io-client'
+import {LineChart,XAxis,Tooltip,CartesianGrid,Line} from 'recharts'
 function MonitorScreen() {
-  const [data, setData] = useState("[]")
+  const [data, setData] = useState([])
   useEffect(()=>{
     const socket = SocketIOClient("http://127.0.0.1:4001/")
     socket.on("message",(data)=>{
       setData(data)
     })
-  },[])
+  },[data])
   return( 
     <div>
-      <h1>{data}</h1>
+      <LineChart width={1000} height={400} data={data} margin={{ top: 5, right: 20, left: 10, bottom: 5 }}>
+      <XAxis dataKey="name" />
+      <Tooltip />
+      <CartesianGrid stroke="#f5f5f5" />
+      <Line type="monotone" dataKey="x" stroke="#ff7300" yAxisId={0} />
+      <Line type="monotone" dataKey="y" stroke="#387908" yAxisId={1} />
+      </LineChart>
     </div>
   )
 }
--- aerobiosys/Devices-Web/src/server/index.mjs
+++ aerobiosys/Devices-Web/src/server/index.mjs
@@ -13,9 +13,21 @@ const server = new socketio.Server(httpServer, {
     }
 })
 let timeChange
+const data = [
+    {name: 1, x: Math.random() * 10, y: Math.random() * 10},
+    {name: 2, x: Math.random() * 10, y: Math.random() * 10},
+    {name: 3, x: Math.random() * 10, y: Math.random() * 10},
+    {name: 4, x: Math.random() * 10, y: Math.random() * 10},
+    {name: 5, x: Math.random() * 10, y: Math.random() * 10}
+]
 server.on("connection",(socket)=>{
     console.log("connected")
     if(timeChange) clearInterval(timeChange)
-    setInterval(()=>socket.emit("message", new Date()),1000)
+    if(data.length > 5){
+        data.reverse().pop()
+        data.reverse()
+    }
+    data.push({name:data[data.length -1].name + 1, x: Math.random()*10, y:Math.random() * 10})
+    setInterval(()=>socket.emit("message",data),1000)
 })
 httpServer.listen(port)
